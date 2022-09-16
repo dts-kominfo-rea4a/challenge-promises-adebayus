a324b9405773c4c0fe6b27d29bbd81cca0bb1d9f
adebayus <adebayus17@gmail.com>
2022-09-16 20:13:10 +0700

    challenge-2

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (param) => {
+
+  let ixx = await promiseTheaterIXX()
+  let vgc = await promiseTheaterVGC()
+  let array = ixx.concat(vgc)
+  let count = array.filter( (item) => item.hasil == param ).length
+  return count
+}
+
 
 module.exports = {
   promiseOutput,
